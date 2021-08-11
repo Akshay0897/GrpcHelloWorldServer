169080b798ec7a27f6605d648ee8978dc6349613
Akshay0897 <akshayaghera05@gmail.com>
2021-08-11 18:23:42 +0530

    initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using Proto.GrpcHelloWorldServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            var grpcClient = new Greeter.GreeterClient(grpcChannel);
+            var reply = await grpcClient.SayHelloAsync (new HelloRequest { Name = "Akshay", LastName = "Patel" });
+            Console.WriteLine(reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
